a475f2b4a44b51acaac21b89adadfe26a90750ea
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-09-04 15:42:42 +0200

    added initial ClientSettings

---
+++ src/forms/client/ClientSettings.tsx
@@ -0,0 +1,41 @@
+import React, { useState, FormEvent } from "react";
+import { ScrollForm } from "../../components/scroll-form/ScrollForm";
+import { useTranslation } from "react-i18next";
+import { ClientDescription } from "./ClientDescription";
+import { ClientRepresentation } from "../../model/client-model";
+import { FormGroup, TextInput } from "@patternfly/react-core";
+
+type ClientSettingsProps = {
+  client: ClientRepresentation;
+};
+
+export const ClientSettings = ({ client: clientInit }: ClientSettingsProps) => {
+  const { t } = useTranslation();
+  const [client, setClient] = useState({ ...clientInit });
+  const onChange = (
+    value: string | boolean,
+    event: FormEvent<HTMLInputElement>
+  ) => {
+    const target = event.target;
+    const name = (target as HTMLInputElement).name;
+
+    setClient({
+      ...client,
+      [name]: value,
+    });
+  };
+  return (
+    <ScrollForm sections={[t("Capability config"), t("General settings")]}>
+      <FormGroup label="Client ID" fieldId="kc-client-id">
+        <TextInput
+          type="text"
+          id="kc-client-id"
+          name="clientId"
+          value={client.clientId}
+          onChange={onChange}
+        />
+      </FormGroup>
+      <ClientDescription client={client} onChange={() => {}} />
+    </ScrollForm>
+  );
+};
