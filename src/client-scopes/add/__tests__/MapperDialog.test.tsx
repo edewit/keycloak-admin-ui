0ce57d878879445966f657d11f431ed466db8ad0
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-10-20 09:43:48 +0200

    format

--- src/client-scopes/add/__tests__/MapperDialog.test.tsx
+++ src/client-scopes/add/__tests__/MapperDialog.test.tsx
@@ -6,21 +6,24 @@ import serverInfo from "../../../context/server-info/__tests__/mock.json";
 import { ServerInfoContext } from "../../../context/server-info/ServerInfoProvider";
 import { AddMapperDialogProps, useAddMapperDialog } from "../MapperDialog";
 
-
 describe("<MapperDialog/>", () => {
   const Test = (args: AddMapperDialogProps) => {
     const [toggle, Dialog] = useAddMapperDialog(args);
     return (
       <ServerInfoContext.Provider value={serverInfo}>
         <Dialog />
-        <Button id="open" onClick={toggle}>Show</Button>
+        <Button id="open" onClick={toggle}>
+          Show
+        </Button>
       </ServerInfoContext.Provider>
     );
-  }
+  };
 
   it("should return empty array when selecting nothing", () => {
     const onConfirm = jest.fn();
-    const container = mount(<Test buildIn={true} protocol="openid-connect" onConfirm={onConfirm}/>);
+    const container = mount(
+      <Test buildIn={true} protocol="openid-connect" onConfirm={onConfirm} />
+    );
 
     container.find("button#open").simulate("click");
     expect(container).toMatchSnapshot();
@@ -32,25 +35,42 @@ describe("<MapperDialog/>", () => {
   it("should return array with selected build in protocol mapping", () => {
     const onConfirm = jest.fn();
     const protocol = "openid-connect";
-    const container = mount(<Test buildIn={true} protocol={protocol} onConfirm={onConfirm}/>);
+    const container = mount(
+      <Test buildIn={true} protocol={protocol} onConfirm={onConfirm} />
+    );
 
     container.find("button#open").simulate("click");
-    container.find("input[name='checkrow0']").simulate("change", { target: { value: true } });
-    container.find("input[name='checkrow1']").simulate("change", { target: { value: true } });
+    container
+      .find("input[name='checkrow0']")
+      .simulate("change", { target: { value: true } });
+    container
+      .find("input[name='checkrow1']")
+      .simulate("change", { target: { value: true } });
 
     container.find("button#modal-confirm").simulate("click");
-    expect(onConfirm).toBeCalledWith([serverInfo.builtinProtocolMappers[protocol][0], serverInfo.builtinProtocolMappers[protocol][1]]);
+    expect(onConfirm).toBeCalledWith([
+      serverInfo.builtinProtocolMappers[protocol][0],
+      serverInfo.builtinProtocolMappers[protocol][1],
+    ]);
   });
 
   it("should return selected protocol mapping type on click", () => {
     const onConfirm = jest.fn();
     const protocol = "openid-connect";
-    const container = mount(<Test buildIn={false} protocol={protocol} onConfirm={onConfirm}/>);
+    const container = mount(
+      <Test buildIn={false} protocol={protocol} onConfirm={onConfirm} />
+    );
 
     container.find("button#open").simulate("click");
     expect(container).toMatchSnapshot();
 
-    container.find("tbody.keycloak__add-mapper-dialog__table-row").find("tr").first().simulate("mousedown");
-    expect(onConfirm).toBeCalledWith(serverInfo.protocolMapperTypes[protocol][0]);
+    container
+      .find("tbody.keycloak__add-mapper-dialog__table-row")
+      .find("tr")
+      .first()
+      .simulate("mousedown");
+    expect(onConfirm).toBeCalledWith(
+      serverInfo.protocolMapperTypes[protocol][0]
+    );
   });
 });
