59b85a58dafed307feeaba6733ecc2585df40a94
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-12-02 15:10:07 +0100

    added js doc

--- src/stories/MockAdminClient.tsx
+++ src/stories/MockAdminClient.tsx
@@ -9,6 +9,19 @@ import { RealmContextProvider } from "../context/realm-context/RealmContext";
 import whoamiMock from "../context/whoami/__tests__/mock-whoami.json";
 import { AdminClient } from "../context/auth/AdminClient";
 
+/**
+ * This component provides some mocked default react context so that other components can work in a storybook.
+ * In it's simplest form wrap your component like so:
+ * @example
+ *  <MockAdminClient>
+ *    <SomeComponent />
+ *  </MockAdminClient>
+ * @example <caption>With an endpoint, roles => findOneById</caption>
+ *   <MockAdminClient mock={{ roles: { findOneById: () => mockJson } }}>
+ *     <<SomeComponent />
+ *   </MockAdminClient>
+ * @param props mock endpoints to be mocked
+ */
 export const MockAdminClient = (props: {
   children: ReactNode;
   mock?: object;
