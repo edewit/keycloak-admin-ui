5e3d1aa9fda63903bc64184bbfbc46e13274e13c
jenny-s51 <jshandel12@gmail.com>
2020-12-02 09:48:06 -0500

    add role attributes, WIP

--- src/realm-roles/RoleAttributes.tsx
+++ src/realm-roles/RoleAttributes.tsx
@@ -1,13 +1,13 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable react/display-name */
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Button, ButtonVariant, TextInput } from "@patternfly/react-core";
-import { useForm } from "react-hook-form";
+import React from "react";
+import {
+  ActionGroup,
+  Button,
+  ButtonVariant,
+  TextInput,
+} from "@patternfly/react-core";
+import { SubmitHandler, useFieldArray, UseFormMethods } from "react-hook-form";
 import "./RealmRolesSection.css";
-import { useAdminClient } from "../context/auth/AdminClient";
 import RoleRepresentation from "keycloak-admin/lib/defs/roleRepresentation";
-import { useTranslation } from "react-i18next";
 
 import {
   TableComposable,
@@ -17,80 +17,117 @@ import {
   Thead,
   Tr,
 } from "@patternfly/react-table";
-import { PlusCircleIcon } from "@patternfly/react-icons";
+import { MinusCircleIcon, PlusCircleIcon } from "@patternfly/react-icons";
+import { useTranslation } from "react-i18next";
+import { FormAccess } from "../components/form-access/FormAccess";
+import { useHistory } from "react-router-dom";
 
-export const RoleAttributes = () => {
+export type KeyValueType = { key: string; value: string }
+
+type RoleAttributesProps = {
+  form: UseFormMethods;
+  save: SubmitHandler<RoleRepresentation>;
+};
+
+export const RoleAttributes = ({ form, save }: RoleAttributesProps) => {
   const { t } = useTranslation("roles");
-  const { setValue } = useForm<RoleRepresentation>();
-  const [, setName] = useState("");
+  const history = useHistory();
 
-  const adminClient = useAdminClient();
-
-  const { id } = useParams<{ id: string }>();
+  const { fields, append, remove } = useFieldArray({control: form.control, name: "attributes"});
 
   const columns = ["Key", "Value"];
-  const rows = [
-    [
-      <TextInput />,
-      <TextInput />,
-      <Button
-        id="kc-plus-icon"
-        variant={ButtonVariant.link}
-        tabIndex={-1}
-        className="kc-role-attributes__plus-icon"
-        aria-label={t("roles:addAttributeText")}
-      >
-        <PlusCircleIcon />
-      </Button>,
-    ],
-  ];
 
-  useEffect(() => {
-    (async () => {
-      const fetchedRole = await adminClient.roles.findOneById({ id });
-      setName(fetchedRole.name!);
-      setupForm(fetchedRole);
-    })();
-  }, []);
-
-  const setupForm = (role: RoleRepresentation) => {
-    Object.entries(role).map((entry) => {
-      setValue(entry[0], entry[1]);
-    });
+  const onAdd = () => {
+    append({key: "", value: ""});
   };
-
+  
   return (
-    <TableComposable
-      className="kc-role-attributes__table"
-      aria-label="Role attribute keys and values"
-      variant="compact"
-      borders={false}
-    >
-      <Thead>
-        <Tr>
-          <Th id="kc-key-label" width={40}>
-            {columns[0]}
-          </Th>
-          <Th id="kc-value-label" width={40}>
-            {columns[1]}
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {rows.map((row, rowIndex) => (
-          <Tr key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <Td
-                key={`${rowIndex}_${cellIndex}`}
-                id={`text-input-${rowIndex}-${cellIndex}`}
-                dataLabel={columns[cellIndex]}
-              >
-                {cell}
-              </Td>
+    <>
+      <FormAccess role="anyone" isHorizontal onSubmit={form.handleSubmit(save)}>
+        <TableComposable
+          className="kc-role-attributes__table"
+          aria-label="Role attribute keys and values"
+          variant="compact"
+          borders={false}
+        >
+          <Thead>
+            <Tr>
+              <Th id="key" width={40}>
+                {columns[0]}
+              </Th>
+              <Th id="value" width={40}>
+                {columns[1]}
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {fields.map((attribute, rowIndex) => (
+              <Tr key={attribute.id}>
+                <Td
+                  key={`${attribute.id}-key`}
+                  id={`text-input-${attribute}-key`}
+                  dataLabel={columns[0]}
+                >
+                  <TextInput
+                    name={`attributes[${rowIndex}].key`}
+                    ref={form.register()}
+                    aria-label="key-input"
+                    defaultValue={attribute.key}
+                  />
+                </Td>
+                <Td
+                  key={`${attribute}-value`}
+                  id={`text-input-${attribute.id}-value`}
+                  dataLabel={columns[1]}
+                >
+                  <TextInput
+                    name={`attributes[${rowIndex}].value`}
+                    ref={form.register()}
+                    aria-label="value-input"
+                    defaultValue={attribute.value}
+                  />
+                </Td>
+                {rowIndex !== fields.length - 1  && fields.length -1 !== 0 && (
+                  <Td key="add-button" id="kc-minus-button" dataLabel={columns[2]}>
+                    <Button
+                      id="minus-icon"
+                      isDisabled={!attribute.key || !attribute.value}
+                      variant={ButtonVariant.link}
+                      tabIndex={-1}
+                      className="kc-role-attributes__plus-icon"
+                      onClick={() => remove(rowIndex)}
+                    >
+                      <MinusCircleIcon />
+                    </Button>
+                  </Td>
+                )}
+                {rowIndex === fields.length - 1 && (
+                  <Td key="add-button" id="add-button" dataLabel={columns[2]}>
+                    <Button
+                      id="plus-icon"
+                      isDisabled={!attribute.key || !attribute.value}
+                      variant={ButtonVariant.link}
+                      tabIndex={-1}
+                      className="kc-role-attributes__plus-icon"
+                      onClick={onAdd}
+                    >
+                      <PlusCircleIcon />
+                    </Button>
+                  </Td>
+                )}
+              </Tr>
             ))}
-          </Tr>
-        ))}
-      </Tbody>
-    </TableComposable>
+          </Tbody>
+        </TableComposable>
+        <ActionGroup className="kc-role-attributes__action-group">
+          <Button variant="primary" type="submit">
+            {t("common:save")}
+          </Button>
+          <Button variant="link" onClick={() => history.push("/roles/")}>
+            {t("common:reload")}
+          </Button>
+        </ActionGroup>
+      </FormAccess>
+    </>
   );
 };
