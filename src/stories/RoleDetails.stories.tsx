958b6dbcc00f5752c162d3cdbc0b3be5484b0699
Erik Jan de Wit <erikjan.dewit@gmail.com>
2020-12-04 09:19:28 +0100

    fix merge error

--- src/stories/RoleDetails.stories.tsx
+++ src/stories/RoleDetails.stories.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Page } from "@patternfly/react-core";
-import { RealmRolesForm } from "../realm-roles/RealmRoleForm";
+import { Meta } from "@storybook/react";
 
 import { MockAdminClient } from "./MockAdminClient";
+import { RealmRolesForm } from "../realm-roles/RealmRoleForm";
 
 import rolesMock from "../realm-roles/__tests__/mock-roles.json";
+import { MemoryRouter, Route } from "react-router-dom";
 
 export default {
   title: "Role details tab",
@@ -15,6 +17,20 @@ export const RoleDetailsExample = () => {
   return (
     <Page>
       <MockAdminClient mock={{ roles: { findOneById: () => rolesMock[0] } }}>
+        <MemoryRouter initialEntries={["/roles/1"]}>
+          <Route path="/roles/:id">
+            <RealmRolesForm />
+          </Route>
+        </MemoryRouter>
+      </MockAdminClient>
+    </Page>
+  );
+};
+
+export const RoleDetailsNew = () => {
+  return (
+    <Page>
+      <MockAdminClient>
         <RealmRolesForm />
       </MockAdminClient>
     </Page>
