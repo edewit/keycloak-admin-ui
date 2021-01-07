fa102e003881fce17e1be3e47010e118cf63b3a7
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-01-07 09:20:36 +0100

    initial version realm settings section

--- src/clients/ClientsSection.tsx
+++ src/clients/ClientsSection.tsx
@@ -11,7 +11,8 @@ import {
 import { ViewHeader } from "../components/view-header/ViewHeader";
 import { useAdminClient } from "../context/auth/AdminClient";
 import { KeycloakDataTable } from "../components/table-toolbar/KeycloakDataTable";
-import { emptyFormatter, exportClient } from "../util";
+import { IFormatter, IFormatterValueType } from "@patternfly/react-table";
+import { emptyFormatter, exportClient, getBaseUrl } from "../util";
 import { useAlerts } from "../components/alert/Alerts";
 import ClientRepresentation from "keycloak-admin/lib/defs/clientRepresentation";
 import { formattedLinkTableCell } from "../components/external-link/FormattedLink";
@@ -23,9 +24,7 @@ export const ClientsSection = () => {
   const { url } = useRouteMatch();
 
   const adminClient = useAdminClient();
-  const baseUrl = adminClient.keycloak
-    ? adminClient.keycloak.authServerUrl!
-    : adminClient.baseUrl + "/";
+  const baseUrl = getBaseUrl(adminClient);
 
   const loader = async (first?: number, max?: number, search?: string) => {
     const params: { [name: string]: string | number } = {
