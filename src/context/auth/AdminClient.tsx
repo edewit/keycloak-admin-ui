877243e98d50642dec13bf02f51101e0d5e8aa04
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-01-08 08:11:28 +0100

    added jsdoc

--- src/context/auth/AdminClient.tsx
+++ src/context/auth/AdminClient.tsx
@@ -17,6 +17,22 @@ export const useAdminClient = () => {
   return adminClient;
 };
 
+/**
+ * Util function to only set the state when the component is still mounted.
+ *
+ * It takes 2 functions one you do your adminClient call in and the other to set your state
+ *
+ * @example
+ * useEffect(() => {
+ *   return asyncStateFetch(
+ *     () => adminClient.components.findOne({ id }),
+ *     (component) => setupForm(component)
+ *   );
+ * }, []);
+ *
+ * @param adminClientCall use this to do your adminClient call
+ * @param callback when the data is fetched this is where you set your state
+ */
 export function asyncStateFetch<T>(
   adminClientCall: () => Promise<T>,
   callback: (param: T) => void
