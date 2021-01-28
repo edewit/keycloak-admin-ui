cb91d6b08cae9dc04d5b0da8ef8331c998b06032
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-01-28 14:17:20 +0100

    when external users then users need to search

--- src/user/UsersSection.tsx
+++ src/user/UsersSection.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import {
   AlertVariant,
@@ -11,11 +11,13 @@ import {
 import { InfoCircleIcon, WarningTriangleIcon } from "@patternfly/react-icons";
 import UserRepresentation from "keycloak-admin/lib/defs/userRepresentation";
 
-import { useAdminClient } from "../context/auth/AdminClient";
+import { asyncStateFetch, useAdminClient } from "../context/auth/AdminClient";
 import { ViewHeader } from "../components/view-header/ViewHeader";
 import { KeycloakDataTable } from "../components/table-toolbar/KeycloakDataTable";
 import { useAlerts } from "../components/alert/Alerts";
 import { RealmContext } from "../context/realm-context/RealmContext";
+import { SearchUser } from "./SearchUser";
+import { ListEmptyState } from "../components/list-empty-state/ListEmptyState";
 
 type BruteUser = UserRepresentation & {
   brute?: Record<string, object>;
@@ -26,19 +28,46 @@ export const UsersSection = () => {
   const adminClient = useAdminClient();
   const { addAlert } = useAlerts();
   const { realm: realmName } = useContext(RealmContext);
+  const [listUsers, setListUsers] = useState(false);
+  const [initialSearch, setInitialSearch] = useState("");
 
   const [key, setKey] = useState("");
   const refresh = () => setKey(`${new Date().getTime()}`);
+
+  useEffect(() => {
+    return asyncStateFetch(
+      () => {
+        const testParams = {
+          type: "org.keycloak.storage.UserStorageProvider",
+        };
+
+        return Promise.all([
+          adminClient.components.find(testParams),
+          adminClient.users.count(),
+        ]);
+      },
+      (response) => {
+        //should *only* list users when no user federation is configured and uses count > 100
+        setListUsers(
+          !((response[0] && response[0].length > 0) || response[1] > 100)
+        );
+      }
+    );
+  }, []);
 
   const loader = async (first?: number, max?: number, search?: string) => {
     const params: { [name: string]: string | number } = {
       first: first!,
       max: max!,
     };
-    if (search) {
-      params.search = search;
+    const searchParam = search || initialSearch || "";
+    if (searchParam) {
+      params.search = searchParam;
     }
 
+    if (!listUsers && !searchParam) {
+      return [];
+    }
     const users = await adminClient.users.find({ ...params });
     const realm = await adminClient.realms.findOne({ realm: realmName });
     if (realm?.bruteForceProtected) {
@@ -89,54 +118,73 @@ export const UsersSection = () => {
     <>
       <ViewHeader titleKey="users:title" subKey="users:userExplain" />
       <PageSection variant="light">
-        <KeycloakDataTable
-          key={key}
-          loader={loader}
-          isPaginated
-          ariaLabelKey="users:title"
-          searchPlaceholderKey="users:searchForUser"
-          toolbarItem={
-            <>
-              <ToolbarItem>
-                <Button>{t("addUser")}</Button>
-              </ToolbarItem>
-              <ToolbarItem>
-                <Button variant={ButtonVariant.plain}>{t("deleteUser")}</Button>
-              </ToolbarItem>
-            </>
-          }
-          actions={[
-            {
-              title: t("common:delete"),
-              onRowClick: (user) => {
-                deleteUser(user);
+        {!listUsers && !initialSearch && (
+          <SearchUser
+            onSearch={(search) => {
+              setInitialSearch(search);
+            }}
+          />
+        )}
+        {(listUsers || initialSearch) && (
+          <KeycloakDataTable
+            key={key}
+            loader={loader}
+            isPaginated
+            ariaLabelKey="users:title"
+            searchPlaceholderKey="users:searchForUser"
+            emptyState={
+              <ListEmptyState
+                message={t("noUsersFound")}
+                instructions={t("emptyInstructions")}
+                primaryActionText={t("createNewUser")}
+                onPrimaryAction={() => {}}
+              />
+            }
+            toolbarItem={
+              <>
+                <ToolbarItem>
+                  <Button>{t("addUser")}</Button>
+                </ToolbarItem>
+                <ToolbarItem>
+                  <Button variant={ButtonVariant.plain}>
+                    {t("deleteUser")}
+                  </Button>
+                </ToolbarItem>
+              </>
+            }
+            actions={[
+              {
+                title: t("common:delete"),
+                onRowClick: (user) => {
+                  deleteUser(user);
+                },
               },
-            },
-          ]}
-          columns={[
-            {
-              name: "username",
-              displayKey: "users:username",
-            },
-            {
-              name: "email",
-              displayKey: "users:email",
-            },
-            {
-              name: "lastName",
-              displayKey: "users:lastName",
-            },
-            {
-              name: "firstName",
-              displayKey: "users:firstName",
-            },
-            {
-              name: "status",
-              displayKey: "users:status",
-              cellRenderer: StatusRow,
-            },
-          ]}
-        />
+            ]}
+            columns={[
+              {
+                name: "username",
+                displayKey: "users:username",
+              },
+              {
+                name: "email",
+                displayKey: "users:email",
+              },
+              {
+                name: "lastName",
+                displayKey: "users:lastName",
+              },
+              {
+                name: "firstName",
+                displayKey: "users:firstName",
+              },
+              {
+                name: "status",
+                displayKey: "users:status",
+                cellRenderer: StatusRow,
+              },
+            ]}
+          />
+        )}
       </PageSection>
     </>
   );
