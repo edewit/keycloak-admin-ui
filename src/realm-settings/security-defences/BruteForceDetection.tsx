f78275f8dfc71785470b1bebf53ca307f231849d
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-07-09 08:22:40 +0200

    Update src/realm-settings/security-defences/BruteForceDetection.tsx
    Co-authored-by: Jenny <32821331+jenny-s51@users.noreply.github.com>

--- src/realm-settings/security-defences/BruteForceDetection.tsx
+++ src/realm-settings/security-defences/BruteForceDetection.tsx
@@ -153,7 +153,7 @@ export const BruteForceDetection = ({
             />
           </FormGroup>
 
-          <Time name="minimumQuickLoginWait" />
+          <Time name="minimumQuickLoginWaitSeconds" />
         </>
       )}
 
