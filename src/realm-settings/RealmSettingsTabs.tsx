6a72c5465ae3e94da0b347d02bc95bfc7a9cb9b1
jenny-s51 <jshandel@redhat.com>
2021-12-08 16:05:15 -0500

    fix lint errors

--- src/realm-settings/RealmSettingsTabs.tsx
+++ src/realm-settings/RealmSettingsTabs.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useHistory, useRouteMatch } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { Controller, FormProvider, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import {
@@ -176,11 +176,6 @@ export const RealmSettingsTabs = ({
   const { refresh: refreshRealms } = useRealms();
   const history = useHistory();
 
-  const match = useRouteMatch();
-
-  const params = match.params as { [index: string]: string };
-  const isLocalizationTabActive = params["tab"] === "localization";
-
   const kpComponentTypes =
     useServerInfo().componentTypes?.[KEY_PROVIDER_TYPE] ?? [];
 
@@ -338,7 +333,6 @@ export const RealmSettingsTabs = ({
             >
               <LocalizationTab
                 key={key}
-                isActive={isLocalizationTabActive}
                 refresh={refresh}
                 save={save}
                 reset={() => resetForm(realm)}
