34ac7d4ab0e3f6a4e0ea11707e837532bdeedf54
jenny-s51 <jshandel@redhat.com>
2021-12-09 16:07:42 -0500

    remove unused onSelect

--- src/realm-settings/RealmSettingsTabs.tsx
+++ src/realm-settings/RealmSettingsTabs.tsx
@@ -329,7 +329,6 @@ export const RealmSettingsTabs = ({
               eventKey="localization"
               data-testid="rs-localization-tab"
               title={<TabTitleText>{t("localization")}</TabTitleText>}
-              onSelect={() => console.log("test")}
             >
               <LocalizationTab
                 key={key}
