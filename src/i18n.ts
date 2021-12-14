8d0643bf54e401b37d3e7d62896ce159c86d5489
Erik Jan de Wit <erikjan.dewit@gmail.com>
2021-12-14 09:54:13 +0100

    use server side translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,76 +1,51 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-// import backend from "i18next-http-backend";
 
-import common from "./common-messages";
-import help from "./common-help";
-import dashboard from "./dashboard/messages";
-import clients from "./clients/messages";
-import clientsHelp from "./clients/help";
-import clientScopes from "./client-scopes/messages";
-import clientScopesHelp from "./client-scopes/help";
-import groups from "./groups/messages";
-import realm from "./realm/messages";
-import roles from "./realm-roles/messages";
-import users from "./user/messages";
-import usersHelp from "./user/help";
-import sessions from "./sessions/messages";
-import events from "./events/messages";
-import realmSettings from "./realm-settings/messages";
-import realmSettingsHelp from "./realm-settings/help";
-import authentication from "./authentication/messages";
-import authenticationHelp from "./authentication/help";
-import userFederation from "./user-federation/messages";
-import userFederationHelp from "./user-federation/help";
-import identityProviders from "./identity-providers/messages";
-import identityProvidersHelp from "./identity-providers/help";
-
-import dynamicLabels from "./components/dynamic/labels";
+import HttpBackend from "i18next-http-backend";
 
 export const DEFAULT_LOCALE = "en";
 
-const initOptions = {
+export const initOptions = {
   defaultNS: "common",
-  resources: {
-    en: {
-      ...common,
-      ...help,
-      ...dashboard,
-      ...clients,
-      ...clientsHelp,
-      ...clientScopes,
-      ...clientScopesHelp,
-      ...groups,
-      ...realm,
-      ...roles,
-      ...groups,
-      ...users,
-      ...usersHelp,
-      ...sessions,
-      ...userFederation,
-      ...events,
-      ...realmSettings,
-      ...realmSettingsHelp,
-      ...authentication,
-      ...authenticationHelp,
-      ...identityProviders,
-      ...identityProvidersHelp,
-      ...userFederation,
-      ...userFederationHelp,
-      ...dynamicLabels,
-    },
-  },
   lng: DEFAULT_LOCALE,
   fallbackLng: DEFAULT_LOCALE,
+  preload: [DEFAULT_LOCALE],
+  ns: [
+    "common",
+    "common-help",
+    "dashboard",
+    "clients",
+    "clients-help",
+    "client-scopes",
+    "client-scopes-help",
+    "groups",
+    "realm",
+    "roles",
+    "users",
+    "users-help",
+    "sessions",
+    "events",
+    "realm-settings",
+    "realm-settings-help",
+    "authentication",
+    "authentication-help",
+    "user-federation",
+    "user-federation-help",
+    "identity-providers",
+    "identity-providers-help",
+    "dynamic",
+  ],
 
   interpolation: {
     escapeValue: false,
   },
+
+  backend: {
+    expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+    loadPath: "/auth/admin/master/console/{{ns}}.json?lang={{lng}}",
+  },
 };
 
-i18n
-  .use(initReactI18next)
-  // .use(backend)
-  .init(initOptions);
+const configuredI18n = i18n.use(initReactI18next).use(HttpBackend);
 
-export default i18n;
+export default configuredI18n;
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "./App";
-import i18n from "./i18n";
+import i18n, { initOptions } from "./i18n";
 import init from "./context/auth/keycloak";
 
-console.info("supported languages", ...i18n.languages);
-
-init().then((adminClient) => {
-  ReactDOM.render(
-    <StrictMode>
-      <App adminClient={adminClient} />
-    </StrictMode>,
-    document.getElementById("app")
-  );
+i18n.init(initOptions, () => {
+  init().then((adminClient) => {
+    ReactDOM.render(
+      <StrictMode>
+        <App adminClient={adminClient} />
+      </StrictMode>,
+      document.getElementById("app")
+    );
+  });
 });
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
