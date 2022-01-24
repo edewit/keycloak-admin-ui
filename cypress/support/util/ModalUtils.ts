a314528c19a3bb0a54a39ed66226e84d6bd89f6f
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-01-24 15:17:55 +0100

    new Client->details->ClientScopes and ClientScopes tests

--- cypress/support/util/ModalUtils.ts
+++ cypress/support/util/ModalUtils.ts
@@ -5,9 +5,18 @@ export default class ModalUtils {
   private confirmModalBtn = "#modal-confirm";
   private cancelModalBtn = "#modal-cancel";
   private closeModalBtn = ".pf-c-modal-box .pf-m-plain";
+  private addModalDropdownBtn = "#add-dropdown > button";
+  private addModalDropdownItem = "#add-dropdown [role='menuitem']";
 
   confirmModal(force = false) {
     cy.get(this.confirmModalBtn).click({ force });
+
+    return this;
+  }
+
+  confirmModalWithItem(itemName: string) {
+    cy.get(this.addModalDropdownBtn).click();
+    cy.get(this.addModalDropdownItem).contains(itemName).click();
 
     return this;
   }
