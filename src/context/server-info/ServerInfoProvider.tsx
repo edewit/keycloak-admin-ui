d3bae3fff1a0b2974689df5776cdbaacf9a664f3
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-05-17 16:27:05 +0200

    fixed test and review comments

--- src/context/server-info/ServerInfoProvider.tsx
+++ src/context/server-info/ServerInfoProvider.tsx
@@ -1,45 +1,24 @@
-import React, {
-  createContext,
-  FunctionComponent,
-  useContext,
-  useState,
-} from "react";
+import React, { createContext, FunctionComponent, useState } from "react";
 
 import type { ServerInfoRepresentation } from "@keycloak/keycloak-admin-client/lib/defs/serverInfoRepesentation";
 import { sortProviders } from "../../util";
 import { useAdminClient, useFetch } from "../auth/AdminClient";
+import useRequiredContext from "../../utils/useRequiredContext";
 
 export const ServerInfoContext = createContext<
   ServerInfoRepresentation | undefined
 >(undefined);
 
-export const useServerInfo = () => {
-  const serverInfo = useContext(ServerInfoContext);
-  if (serverInfo === undefined) {
-    throw new Error("Forbidden");
-  }
-  return serverInfo;
-};
+export const useServerInfo = () => useRequiredContext(ServerInfoContext);
 
-export const useLoginProviders = () => {
-  return sortProviders(useServerInfo().providers!["login-protocol"].providers);
-};
+export const useLoginProviders = () =>
+  sortProviders(useServerInfo().providers!["login-protocol"].providers);
 
 export const ServerInfoProvider: FunctionComponent = ({ children }) => {
   const adminClient = useAdminClient();
-  const [serverInfo, setServerInfo] = useState<ServerInfoRepresentation>();
+  const [serverInfo, setServerInfo] = useState<ServerInfoRepresentation>({});
 
-  useFetch(
-    async () => {
-      try {
-        return await adminClient.serverInfo.find();
-      } catch (error) {
-        return undefined;
-      }
-    },
-    setServerInfo,
-    []
-  );
+  useFetch(adminClient.serverInfo.find, setServerInfo, []);
 
   return (
     <ServerInfoContext.Provider value={serverInfo}>
