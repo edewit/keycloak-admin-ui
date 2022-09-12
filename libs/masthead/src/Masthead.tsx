396035691302d199a0f7941a4c02c689aac12101
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-09-12 12:02:29 +0200

    added initial version of masthead lib

---
+++ libs/masthead/src/Masthead.tsx
@@ -0,0 +1,70 @@
+import { ReactNode } from "react";
+import {
+  Avatar,
+  AvatarProps,
+  Brand,
+  BrandProps,
+  PageHeader,
+  PageHeaderProps,
+  PageHeaderTools,
+  PageHeaderToolsGroup,
+  PageHeaderToolsItem,
+} from "@patternfly/react-core";
+
+import { KeycloakDropdown } from "./KeycloakDropdown";
+
+type BrandLogo = BrandProps & {
+  onClick?: () => void;
+};
+
+type KeycloakMastheadProps = PageHeaderProps & {
+  brand: BrandLogo;
+  avatar?: AvatarProps;
+  kebabDropdownItems?: ReactNode[];
+  dropdownItems: ReactNode[];
+};
+
+export const KeycloakMasthead = ({
+  brand: { onClick: onBrandLogoClick, ...brandProps },
+  avatar,
+  kebabDropdownItems,
+  dropdownItems,
+  ...rest
+}: KeycloakMastheadProps) => {
+  return (
+    <PageHeader
+      {...rest}
+      logo={
+        <div onClick={onBrandLogoClick}>
+          <Brand {...brandProps} />
+        </div>
+      }
+      logoComponent="div"
+      headerTools={
+        <PageHeaderTools>
+          <PageHeaderToolsGroup>
+            <PageHeaderToolsItem
+              visibility={{
+                md: "hidden",
+              }} /** this kebab dropdown replaces the icon buttons and is hidden for desktop sizes */
+            >
+              <KeycloakDropdown
+                isKebab
+                dropDownItems={kebabDropdownItems || dropdownItems}
+              />
+            </PageHeaderToolsItem>
+            <PageHeaderToolsItem
+              visibility={{
+                default: "hidden",
+                md: "visible",
+              }} /** this user dropdown is hidden on mobile sizes */
+            >
+              <KeycloakDropdown dropDownItems={dropdownItems} />
+            </PageHeaderToolsItem>
+          </PageHeaderToolsGroup>
+          <Avatar {...{ src: "/avatar.svg", alt: "avatar", ...avatar }} />
+        </PageHeaderTools>
+      }
+    />
+  );
+};
