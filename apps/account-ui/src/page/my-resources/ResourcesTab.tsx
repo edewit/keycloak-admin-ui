91705f4611cf69e7c82e7559bb1d7a176581d03d
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-10-03 14:14:17 +0200

    added share the resource + collapse

--- apps/account-ui/src/page/my-resources/ResourcesTab.tsx
+++ apps/account-ui/src/page/my-resources/ResourcesTab.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Spinner } from "@patternfly/react-core";
-import { ExternalLinkAltIcon } from "@patternfly/react-icons";
+import {
+  Button,
+  Dropdown,
+  KebabToggle,
+  OverflowMenu,
+  OverflowMenuContent,
+  OverflowMenuControl,
+  OverflowMenuDropdownItem,
+  OverflowMenuGroup,
+  OverflowMenuItem,
+  Spinner,
+} from "@patternfly/react-core";
+import { ExternalLinkAltIcon, ShareAltIcon } from "@patternfly/react-icons";
 import {
   TableComposable,
   Thead,
@@ -9,13 +20,23 @@ import {
   Th,
   Tbody,
   Td,
+  ExpandableRowContent,
 } from "@patternfly/react-table";
 
 import { useAccountClient, useFetch } from "../../context/fetch";
-import { Resource } from "../../representations";
+import { Permission, Resource } from "../../representations";
 import { Links } from "../../context/parse-links";
 import { ResourceToolbar } from "./ResourceToolbar";
 import { PermissionRequest } from "./PermissionRequest";
+import { ShareTheResource } from "./ShareTheResource";
+import { SharedWith } from "./SharedWith";
+
+type PermissionDetail = {
+  contextOpen?: boolean;
+  rowOpen?: boolean;
+  dialogOpen?: boolean;
+  permissions?: Permission[];
+};
 
 export const ResourcesTab = () => {
   const { t } = useTranslation();
@@ -27,7 +48,11 @@ export const ResourcesTab = () => {
   });
   const [links, setLinks] = useState<Links | undefined>();
   const [resources, setResources] = useState<Resource[]>();
+  const [details, setDetails] = useState<
+    Record<string, PermissionDetail | undefined>
+  >({});
   const [key, setKey] = useState(1);
+  const refresh = () => setKey(key + 1);
 
   useFetch(
     async (signal) => {
@@ -54,6 +79,40 @@ export const ResourcesTab = () => {
     return <Spinner />;
   }
 
+  const fetchPermissions = async (id: string) => {
+    let permissions = details[id]?.permissions || [];
+    if (!details[id]) {
+      permissions = await accountClient.fetchPermission({}, id);
+    }
+    return permissions;
+  };
+
+  const toggleRowOpen = async (id: string, open: boolean) => {
+    const permissions = await fetchPermissions(id);
+
+    setDetails({
+      ...details,
+      [id]: { ...details[id], rowOpen: open, permissions },
+    });
+  };
+
+  const toggleContextOpen = async (id: string, open: boolean) => {
+    const permissions = await fetchPermissions(id);
+
+    setDetails({
+      ...details,
+      [id]: { ...details[id], contextOpen: open, permissions },
+    });
+  };
+
+  const toggleDialogOpen = async (id: string, open: boolean) => {
+    const permissions = await fetchPermissions(id);
+
+    setDetails({
+      ...details,
+      [id]: { ...details[id], dialogOpen: open, permissions },
+    });
+  };
   return (
     <>
       <ResourceToolbar
@@ -71,32 +130,109 @@ export const ResourcesTab = () => {
       <TableComposable aria-label={t("resources")}>
         <Thead>
           <Tr>
+            <Th />
             <Th>{t("resourceName")}</Th>
             <Th>{t("application")}</Th>
             <Th>{t("permissionRequests")}</Th>
           </Tr>
         </Thead>
-        <Tbody>
-          {resources.map((resource) => (
-            <Tr key={resource.name}>
-              <Td dataLabel="resourceName">{resource.name}</Td>
-              <Td dataLabel="application">
+        {resources.map((resource, index) => (
+          <Tbody
+            key={resource.name}
+            isExpanded={details[resource._id]?.rowOpen}
+          >
+            <Tr>
+              <Td
+                expand={{
+                  isExpanded: details[resource._id]?.rowOpen || false,
+                  rowIndex: index,
+                  onToggle: () =>
+                    toggleRowOpen(
+                      resource._id,
+                      !details[resource._id]?.rowOpen
+                    ),
+                }}
+              />
+              <Td dataLabel={t("resourceName")}>{resource.name}</Td>
+              <Td dataLabel={t("application")}>
                 <a href={resource.client.baseUrl}>
                   {resource.client.name || resource.client.clientId}{" "}
                   <ExternalLinkAltIcon />
                 </a>
               </Td>
-              <Td dataLabel="permissionRequests">
+              <Td dataLabel={t("permissionRequests")}>
                 {resource.shareRequests.length > 0 && (
                   <PermissionRequest
                     resource={resource}
-                    refresh={() => setKey(key + 1)}
+                    refresh={() => refresh()}
                   />
                 )}
+                <ShareTheResource
+                  resource={resource}
+                  permissions={details[resource._id]?.permissions}
+                  open={details[resource._id]?.dialogOpen || false}
+                  onClose={() => setDetails({})}
+                />
+              </Td>
+              <Td isActionCell>
+                <OverflowMenu breakpoint="lg">
+                  <OverflowMenuContent>
+                    <OverflowMenuGroup groupType="button">
+                      <OverflowMenuItem>
+                        <Button
+                          variant="link"
+                          onClick={() => toggleDialogOpen(resource._id, true)}
+                        >
+                          <ShareAltIcon /> {t("share")}
+                        </Button>
+                      </OverflowMenuItem>
+                      <OverflowMenuItem>
+                        <Button variant="secondary">Secondary</Button>
+                      </OverflowMenuItem>
+                      <OverflowMenuItem>
+                        <Button variant="tertiary">Tertiary</Button>
+                      </OverflowMenuItem>
+                    </OverflowMenuGroup>
+                  </OverflowMenuContent>
+                  <OverflowMenuControl>
+                    <Dropdown
+                      position="right"
+                      toggle={
+                        <KebabToggle
+                          onToggle={(open) =>
+                            toggleContextOpen(resource._id, open)
+                          }
+                        />
+                      }
+                      isOpen={details[resource._id]?.contextOpen}
+                      isPlain
+                      dropdownItems={[
+                        <OverflowMenuDropdownItem key="item1" isShared>
+                          <ShareAltIcon /> {t("share")}
+                        </OverflowMenuDropdownItem>,
+                        <OverflowMenuDropdownItem key="item2" isShared>
+                          Secondary
+                        </OverflowMenuDropdownItem>,
+                        <OverflowMenuDropdownItem key="item3" isShared>
+                          Tertiary
+                        </OverflowMenuDropdownItem>,
+                      ]}
+                    />
+                  </OverflowMenuControl>
+                </OverflowMenu>
               </Td>
             </Tr>
-          ))}
-        </Tbody>
+            <Tr isExpanded={details[resource._id]?.rowOpen || false}>
+              <Td colSpan={4} textCenter>
+                <ExpandableRowContent>
+                  <SharedWith
+                    permissions={details[resource._id]?.permissions}
+                  />
+                </ExpandableRowContent>
+              </Td>
+            </Tr>
+          </Tbody>
+        ))}
       </TableComposable>
     </>
   );
---
+++ apps/account-ui/src/page/my-resources/ShareTheResource.tsx
@@ -0,0 +1,165 @@
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
+import {
+  Modal,
+  Button,
+  Form,
+  FormGroup,
+  ChipGroup,
+  Chip,
+  InputGroup,
+  ValidatedOptions,
+} from "@patternfly/react-core";
+
+import { Permission, Resource } from "../../representations";
+import { KeycloakTextInput } from "../components/keycloak-text-input/KeycloakTextInput";
+import { FormSelect } from "../components/form-select/FormSelect";
+import { SharedWith } from "./SharedWith";
+import { useAccountClient } from "../../context/fetch";
+import { useAlerts } from "../../context/alerts";
+
+type ShareTheResourceProps = {
+  resource: Resource;
+  permissions?: Permission[];
+  open: boolean;
+  onClose: () => void;
+};
+
+type FormValues = {
+  permissions: string[];
+  usernames: { value: string }[];
+};
+
+export const ShareTheResource = ({
+  resource,
+  permissions,
+  open,
+  onClose,
+}: ShareTheResourceProps) => {
+  const { t } = useTranslation();
+  const accountClient = useAccountClient();
+  const { addAlert, addError } = useAlerts();
+  const form = useForm<FormValues>({ mode: "onChange" });
+  const {
+    control,
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = form;
+  const { fields, append, remove } = useFieldArray<FormValues>({
+    control,
+    name: "usernames",
+  });
+
+  useEffect(() => {
+    if (fields.length === 0) {
+      append({ value: "" });
+    }
+  }, [fields]);
+
+  const addShare = async ({ usernames, permissions }: FormValues) => {
+    try {
+      await Promise.all(
+        usernames.map(({ value: username }) =>
+          accountClient.updateRequest(resource._id, username, permissions)
+        )
+      );
+      addAlert("shareSuccess");
+      onClose();
+    } catch (error) {
+      addError("shareError", error);
+    }
+  };
+
+  return (
+    <Modal
+      title={t("shareTheResourceTitle", [resource.name])}
+      variant="medium"
+      isOpen={open}
+      onClose={onClose}
+      actions={[
+        <Button
+          key="confirm"
+          variant="primary"
+          id="done"
+          isDisabled={!isValid}
+          type="submit"
+          form="share-form"
+        >
+          {t("done")}
+        </Button>,
+        <Button key="cancel" variant="link" onClick={onClose}>
+          {t("cancel")}
+        </Button>,
+      ]}
+    >
+      <Form id="share-form" onSubmit={handleSubmit(addShare)}>
+        <FormGroup
+          label={t("shareUserLabel")}
+          type="string"
+          helperTextInvalid={t("resourceAlreadyShared")}
+          fieldId="users"
+          isRequired
+          validated={
+            errors.usernames ? ValidatedOptions.error : ValidatedOptions.default
+          }
+        >
+          <InputGroup>
+            <KeycloakTextInput
+              id="users"
+              placeholder={t("usernamePlaceholder")}
+              validated={
+                errors.usernames
+                  ? ValidatedOptions.error
+                  : ValidatedOptions.default
+              }
+              {...register(`usernames.${fields.length - 1}.value`, {
+                required: true,
+                validate: () => fields.length > 0,
+              })}
+            />
+            <Button
+              key="add-user"
+              variant="primary"
+              id="add"
+              onClick={() => append({ value: "" })}
+              isDisabled={!isValid}
+            >
+              {t("add")}
+            </Button>
+          </InputGroup>
+          {fields.length > 1 && (
+            <ChipGroup categoryName={t("shareWith")}>
+              {fields.map(
+                (field, index) =>
+                  index !== fields.length - 1 && (
+                    <Chip key={field.id} onClick={() => remove(index)}>
+                      {field.value}
+                    </Chip>
+                  )
+              )}
+            </ChipGroup>
+          )}
+        </FormGroup>
+        <FormProvider {...form}>
+          <FormGroup label="" fieldId="permissions-selected">
+            <FormSelect
+              name="permissions"
+              variant="typeaheadmulti"
+              controller={{ defaultValue: [] }}
+              options={resource.scopes.map(({ name, displayName }) => ({
+                key: name,
+                value: displayName || name,
+              }))}
+              menuAppendTo="parent"
+            />
+          </FormGroup>
+        </FormProvider>
+        <FormGroup>
+          <SharedWith permissions={permissions} />
+        </FormGroup>
+      </Form>
+    </Modal>
+  );
+};
