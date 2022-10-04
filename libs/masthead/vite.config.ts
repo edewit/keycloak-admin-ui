b183ac5b6bb40ff0964142c6833ee56d4c68b83f
Jon Koops <jonkoops@gmail.com>
2022-10-04 12:43:08 +0200

    Clean up Vite config for Masthead

--- libs/masthead/vite.config.ts
+++ libs/masthead/vite.config.ts
@@ -1,20 +1,13 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
+    // Resolve the 'module' entrypoint at all times (not the default due to Node.js compatibility issues).
     mainFields: ["module"],
     dedupe: ["react", "react-dom"],
   },
   plugins: [react(), checker({ typescript: true })],
-  test: {
-    setupFiles: "vitest.setup.ts",
-    watch: false,
-    deps: {
-      // Ensure '.mjs' files are used for '@patternfly/react-styles'.
-      inline: [/@patternfly\/react-styles/],
-    },
-  },
 });
