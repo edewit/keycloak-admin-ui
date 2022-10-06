d6da60d5735fd3934fa7be27f2a4dff987c45d5d
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-10-06 08:45:27 +0200

    added credential table for user federation
    same as in the old console when credentials
    are federated by ldap

--- apps/admin-ui/src/user/UserCredentials.tsx
+++ apps/admin-ui/src/user/UserCredentials.tsx
@@ -37,6 +37,7 @@ import { CredentialRow } from "./user-credentials/CredentialRow";
 import { toUpperCase } from "../util";
 
 import "./user-credentials.css";
+import { FederatedCredentials } from "./user-credentials/FederatedCredentials";
 
 type UserCredentialsProps = {
   user: UserRepresentation;
@@ -361,7 +362,7 @@ export const UserCredentials = ({ user }: UserCredentialsProps) => {
           <Divider />
         </>
       )}
-      {groupedUserCredentials.length !== 0 ? (
+      {groupedUserCredentials.length !== 0 && (
         <>
           {user.email && (
             <Button
@@ -490,26 +491,31 @@ export const UserCredentials = ({ user }: UserCredentialsProps) => {
             </TableComposable>
           </PageSection>
         </>
-      ) : (
-        <ListEmptyState
-          hasIcon={true}
-          message={t("noCredentials")}
-          instructions={t("noCredentialsText")}
-          primaryActionText={t("setPassword")}
-          onPrimaryAction={toggleModal}
-          secondaryActions={
-            user.email
-              ? [
-                  {
-                    text: t("credentialResetBtn"),
-                    onClick: toggleCredentialsResetModal,
-                    type: ButtonVariant.link,
-                  },
-                ]
-              : undefined
-          }
-        />
       )}
+      {(user.federationLink || user.origin) && (
+        <FederatedCredentials user={user} onSetPassword={toggleModal} />
+      )}
+      {groupedUserCredentials.length === 0 &&
+        !(user.federationLink || user.origin) && (
+          <ListEmptyState
+            hasIcon={true}
+            message={t("noCredentials")}
+            instructions={t("noCredentialsText")}
+            primaryActionText={t("setPassword")}
+            onPrimaryAction={toggleModal}
+            secondaryActions={
+              user.email
+                ? [
+                    {
+                      text: t("credentialResetBtn"),
+                      onClick: toggleCredentialsResetModal,
+                      type: ButtonVariant.link,
+                    },
+                  ]
+                : undefined
+            }
+          />
+        )}
     </>
   );
 };
