a8b4a1a746a7a1625784b30ab0d0ca769bea43d7
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-11-03 08:27:39 +0100

    select

--- apps/admin-ui/src/user/UserProfileFields.tsx
+++ apps/admin-ui/src/user/UserProfileFields.tsx
@@ -1,17 +1,31 @@
 import { useTranslation } from "react-i18next";
-import { useFormContext } from "react-hook-form";
-import { Form, FormGroup } from "@patternfly/react-core";
+import { Controller, useFormContext } from "react-hook-form";
+import { Form, FormGroup, Select } from "@patternfly/react-core";
 
+import type { UserProfileAttribute } from "@keycloak/keycloak-admin-client/lib/defs/userProfileConfig";
 import { KeycloakTextInput } from "../components/keycloak-text-input/KeycloakTextInput";
+import useToggle from "../utils/useToggle";
 import { useUserProfile } from "../realm-settings/user-profile/UserProfileContext";
+
+const ROOT_ATTRIBUTES = ["username", "fistName", "lastName", "email"];
 
 export const UserProfileFields = () => {
   const { t } = useTranslation("users");
   const { config } = useUserProfile();
-  const { errors, register } = useFormContext();
+  const { errors, register, control } = useFormContext();
+  const [open, toggle] = useToggle();
 
   const isBundleKey = (displayName?: string) => displayName?.includes("${");
   const unWrap = (key: string) => key.substring(2, key.length - 1);
+
+  const isRootAttribute = (attr?: string) =>
+    attr && ROOT_ATTRIBUTES.includes(attr);
+
+  const isSelect = (attribute: UserProfileAttribute) =>
+    attribute.validations?.filter((validation) => validation.options);
+
+  const fieldName = (attribute: UserProfileAttribute) =>
+    `${isRootAttribute(attribute.name) ? "" : "attribute."}${attribute.name}`;
 
   return (
     <Form>
@@ -23,18 +37,47 @@ export const UserProfileFields = () => {
               ? t(unWrap(attribute.displayName!))
               : attribute.displayName
           }
-          fieldId="kc-username"
-          isRequired
+          fieldId={attribute.name}
+          isRequired={attribute.required !== undefined}
           validated={errors.username ? "error" : "default"}
           helperTextInvalid={t("common:required")}
         >
-          <KeycloakTextInput
-            ref={register()}
-            type="text"
-            id="kc-username"
-            aria-label={t("username")}
-            name={`attributes.${attribute.name}`}
-          />
+          {isSelect(attribute) ? (
+            <Controller
+              name="bindingType"
+              defaultValue={"browserFlow"}
+              control={control}
+              render={({ onChange, value }) => (
+                <Select
+                  toggleId="chooseBindingType"
+                  onToggle={toggle}
+                  onSelect={(_, value) => {
+                    onChange(value.toString());
+                    toggle();
+                  }}
+                  selections={value}
+                  variant="single"
+                  aria-label={t("bindingFlow")}
+                  isOpen={open}
+                  menuAppendTo="parent"
+                >
+                  {/* {(attribute.validations?. as string[]).map((option) => (
+                    <SelectOption selected={value === option} key={option}>
+                      {option}
+                    </SelectOption>
+                  ))} */}
+                </Select>
+              )}
+            />
+          ) : (
+            <KeycloakTextInput
+              ref={register()}
+              type="text"
+              id={attribute.name}
+              aria-label={t("username")}
+              name={fieldName(attribute)}
+            />
+          )}
         </FormGroup>
       ))}
     </Form>
